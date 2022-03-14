c12e7c04d5810848f7af54b479d104f1c2f8b956
Jan Ryklikas <jry@facelift-bbt.com>
2022-03-14 23:15:57 +0100

    move /data to /share

---
+++ omada-5/CHANGELOG.md
@@ -0,0 +1,9 @@
+<!-- https://developers.home-assistant.io/docs/add-ons/presentation#keeping-a-changelog -->
+
+## 1.2.0
+
+- change persistance to "/share" folder
+
+## 1.0.0
+
+- Initial release
--- omada-5/init.sh
+++ omada-5/init.sh
@@ -11,7 +11,7 @@ bashio::log.info "[init] Setup Omada Dir"
 mkdir -p ${OMADA_PERSIST}
 # ------------------------------------
 bashio::log.info "[init] Setup omada persistent folder"
-mkdir "${OMADA_PERSIST}/data" "${OMADA_PERSIST}/logs" "${OMADA_PERSIST}/work"
+mkdir -p "${OMADA_PERSIST}/data/db" "${OMADA_PERSIST}/logs" "${OMADA_PERSIST}/work"
 # ------------------------------------
 bashio::log.info "[init] Prepare Logs"
 touch ${OMADA_PERSIST}/logs/server.log
@@ -21,8 +21,5 @@ bashio::log.info "[init] Set omada permissions"
 chown -R omada:omada "${OMADA_PERSIST}"
 # ------------------------------------
 bashio::log.info "[init] Copy folder contents"
-cp -a ${OMADA_DIR}/data/. ${OMADA_PERSIST}/data/
-cp -a ${OMADA_DIR}/work/. ${OMADA_PERSIST}/work/
-cp -a ${OMADA_DIR}/logs/. ${OMADA_PERSIST}/logs/
 # ------------------------------------
 bashio::log.info "[init] Done"
